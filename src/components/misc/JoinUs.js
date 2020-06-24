2d9f52f7684efe4684b67bbb22416702dfc9d0a9
royhadad <royhadad98@gmail.com>
2020-06-24 15:39:49 +0300

    finished footer and JoinUs component

--- src/components/misc/JoinUs.js
+++ src/components/misc/JoinUs.js
@@ -1,16 +1,56 @@
 import React from 'react';
+import signupUser from '../../utils/signupUser';
+import validator from 'validator';
 import miscResources from '../../resources/components/misc.json';
 const resources = miscResources.JoinUs;
 
-export default () => {
-    return (
-        <div className='join-us-container'>
-            <div className='join-us-header'>
+class JoinUs extends React.Component {
+    state = {}
+    constructor(props) {
+        super(props);
+        this.user_name_ref = React.createRef();
+        this.user_email_ref = React.createRef();
+    }
 
-            </div>
-            <div className='form-container'>
+    componentDidMount() {
+        this.setState(() => ({ error: undefined }))
+    }
 
+    onSubmit = (e) => {
+        e.stopPropagation();
+        const name = this.user_name_ref.current.value;
+        const email = this.user_email_ref.current.value;
+        if (!validator.isEmail(email)) {
+            this.setState(() => ({ error: resources.emailError }));
+        } else if (validator.isEmpty(name)) {
+            this.setState(() => ({ error: resources.nameError }));
+        } else {
+            this.setState(() => ({ error: resources.successText }));
+            signupUser(name, email);
+        }
+    }
+
+    render() {
+        return (
+            <div className='join-us-container'>
+                <div className='join-us-inner-container'>
+                    <div className='join-us-header'>
+                        {resources.header}
+                    </div>
+                    <div className='form-container'>
+                        <input type='text' placeholder={resources.namePlaceholder} ref={this.user_name_ref} />
+                        <input type='text' placeholder={resources.emailPlaceholder} ref={this.user_email_ref} />
+                        <button className='signup-button' onClick={this.onSubmit}>
+                            {resources.signupButtonText}
+                        </button>
+                    </div>
+                    <div className='join-us-error' style={this.state.error === resources.successText ? { color: 'green' } : {}}>
+                        {this.state.error}
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
+export default JoinUs;
---
+++ src/utils/signupUser.js
@@ -0,0 +1,3 @@
+export default (name, email) => {
+    console.log(`Should add to customers DB ${JSON.stringify({ name, email })}`);
+}
