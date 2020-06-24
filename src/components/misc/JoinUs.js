556503d895b8ed414330ab7cc5acccd4e8617c63
royhadad <royhadad98@gmail.com>
2020-06-24 13:28:53 +0300

    added basic node server to serve the app for production

--- src/components/misc/JoinUs.js
+++ src/components/misc/JoinUs.js
@@ -5,7 +5,12 @@ const resources = miscResources.JoinUs;
 export default () => {
     return (
         <div className='join-us-container'>
+            <div className='join-us-header'>
 
+            </div>
+            <div className='form-container'>
+
+            </div>
         </div>
     )
 }
